7628a2532fe7ad81da4a8b0ae6c4444f02a1aa17
Rapheal Chizitere <www.rashyking05@gmail.com>
2026-03-30 01:38:35 +0100

    add: move utils.h and utils.c to challenges directory and implement weight conversion program

---
+++ include/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Reads a line from stdin and removes the trailing newline character.
+ */
+void fgets_and_strlen(char *str, size_t size);
+
+#endif
