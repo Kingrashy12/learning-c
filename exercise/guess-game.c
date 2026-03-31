e4dc6aa48a8a23bc1b49223fd58e1e32253edb5a
Rapheal Chizitere <www.rashyking05@gmail.com>
2026-03-31 18:42:34 +0100

    add: implement guess the number game with user input and feedback

--- exercise/guess-game.c
+++ exercise/guess-game.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
 int main()
 {
+
+    SetConsoleOutputCP(CP_UTF8); // Set console output code page to UTF-8
 
     int tries = 0;
     int guess = 0;
@@ -15,6 +18,26 @@ int main()
 
     printf("🎮 Welcome to Guess the Number!\n");
     printf("I'm thinking of a number between %d - %d.\n", min, max);
+
+    while (guess != answer)
+    {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        tries++;
+
+        if (guess < answer)
+        {
+            printf("Too low! Try again.\n");
+        }
+        else if (guess > answer)
+        {
+            printf("Too high! Try again.\n");
+        }
+        else
+        {
+            printf("Congratulations! You've guessed the number in %d tries!\n", tries);
+        }
+    }
 
     return 0;
 }
