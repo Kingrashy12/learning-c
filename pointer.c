d3a39715316062b1806eeb859c4e5eacd00208f9
Rapheal Chizitere <www.rashyking05@gmail.com>
2026-04-04 01:40:19 +0100

    add: implement callback functions and demonstrate usage in main

--- pointer.c
+++ pointer.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 void birthday(int *age);
+int add(int a, int b);
+void greetMorning();
+void greetEvening();
+void greet(void (*func)());
+void runCallback(void (*callback)());
+void sayHello();
+void calculate(void (*callback)(int, int), int x, int y);
+void addNumbers(int a, int b);
 
 int main()
 
@@ -13,10 +21,56 @@ int main()
     birthday(&age);
     printf("Age after birthday: %d\n", age);
 
+    int (*ptr)(int, int) = add;
+    int result = ptr(5, 3);
+    printf("Result: %d\n", result);
+
+    greet(greetMorning);
+    greet(greetEvening);
+
+    runCallback(sayHello);
+
+    calculate(addNumbers, 5, 3);
+
     return 0;
 }
 
 void birthday(int *age)
 {
     (*age)++;
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+void greetMorning() { printf("Good morning!\n"); }
+void greetEvening() { printf("Good evening!\n"); }
+
+void greet(void (*func)())
+{
+    func();
+}
+
+void sayHello()
+{
+    printf("Hello from the callback!\n");
+}
+
+void runCallback(void (*callback)())
+{
+    printf("Before calling the callback...\n");
+    callback();
+    printf("After calling the callback.\n");
+}
+
+void addNumbers(int a, int b)
+{
+    printf("The sum is: %d\n", a + b);
+}
+
+void calculate(void (*callback)(int, int), int x, int y)
+{
+    callback(x, y);
 }
